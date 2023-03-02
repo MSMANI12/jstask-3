37577a36d51fc498a66e279ae1ee7a69bc4f87e7
MSMANI12 <selvamani12121998@gmail.com>
2023-03-02 14:21:02 +0530

    add jstask-3

---
+++ main.js
@@ -0,0 +1,12 @@
+var oddNum=parseInt(prompt('senter a number:'));
+ function odd(oddNum){
+   if(oddNum%2!==0){
+      console.log("odd number");
+	  }
+	  else{
+	        console.log("even number");
+	  }
+	  return oddNum;
+	 
+}
+      console.log(odd(oddNum));
